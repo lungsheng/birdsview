9713e7be286a9126c9f821047d350b383819f2b2
lungsheng <dragonenator@gmail.com>
2020-01-20 19:04:19 +0800

    first commit

---
+++ show.js
@@ -0,0 +1,172 @@
+var s=[
+   {
+    "caption" : "白腰鵲鴝（ㄑㄩˊ），一種非常美麗的鳥，屬鶲科鵲鴝屬，其原生地在中國南方及東南亞地區，在台灣為外來種，因其鮮艷的外表，優美、多變的鳴聲，甚至還會模仿其他鳥類的聲音，而被引進做為竉物，但終究會有被野放或逃出的，逸出的個體在野外繁殖，1988年在台北植物園首次被發現，這幾年主要都在低海拔地區的竹林及次生闊葉林活動。腰鵲鴝雖然美麗，但生性強悍，領域性強，就跟家八哥等外來鳥類一樣，易與原生物種產生排擠效應，也威脅生態的平衡，所以也是令人頭痛的鳥類。",
+    "name" : "白腰鵲鴝",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN1175.JPG?alt=media&token=966d8877-8aa2-4a57-9c64-4dde7cb528fe"
+  },
+   {
+    "caption" : "黃鸝:全身鮮黃色、嘴桃紅色的中型林鳥。眼暗褐色，過眼線黑色向後延伸至後頭，飛羽大致黑色，尾羽黑色，兩側末端鮮黃色。雌鳥的顏色較淡，過眼線的寬度較窄，腳灰黑色。出現在低海拔地區次生林頂層。在台江地區有留鳥和遷徒性候鳥族群，數量稀少，通常單獨或成對活動，在鹽水溪口及曾文溪口的防風林都曾有過紀錄。以植物果實為主要食物，叫聲嘹亮動聽，加上外形鮮豔，早年被獵捕壓力很大。",
+    "name" : "黃鸝",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN4144.JPG?alt=media&token=7bf6cc05-ec2d-4435-bf72-fbe405fe28ad"
+  },
+ {
+    "caption" : "冠羽畫眉:台灣特有種鳥類冠羽畫眉尚稱普遍，分布於中、高海拔的闊葉林及針 闊葉混合林，常成群活動於樹冠，啄食昆蟲及果實，極為活潑、嘈雜。牠們「吐米酒」、「吐米酒」的叫聲，讓你無法忽略牠們的存在。冬 季時常降遷到數百公尺的闊葉林內，因此冬季時在台北近郊的烏來也可能 觀察得到牠們。暗褐色高聳的羽冠，是牠名字的由來。據日本鳥類專家山階芳磨的觀 察，冠羽畫眉有群聚繁殖的習性，當繁殖時，數對不同親鳥會產卵於同一 巢中，並共同餵食幼雛。",
+    "name" : "冠羽畫眉",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN4493.JPG?alt=media&token=115ed241-fb5b-48b7-a054-025ba6952196"
+  },
+  {
+    "caption" : "白耳畫眉:\n大型畫眉鳥。體背與上胸為灰黑色，腹部為淡橘色，白色過眼現甚長延伸至耳羽、後頭側。黑色尾羽末端有白斑。聲音遼亮。留鳥。主要棲息於山區樹林中，喜鳴唱，不善飛行。喜於濃密樹上、草叢間活動。出現於闊葉林、混合林。非繁殖季喜歡集結成群，也與其它種類混群。主要食物為昆蟲、種子、果實。海拔分布於200至2500公尺",
+    "name" : "白耳畫眉",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN4503.JPG?alt=media&token=302e7e0b-0302-47da-a445-8411c06fffb3"
+  },
+  {
+    "caption" : "棕面鶯:身長約九至十公分，為普遍的留鳥，喜歡棲息於中、高海拔山區，常和山雀科或小型畫眉科小鳥成群活動，在雜木林或竹林的中、上層枝椏間穿梭捕食小型昆蟲或其幼蟲。其鳴聲清晰輕細，如銀鈴聲一般。",
+    "name" : "棕面鶯",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN1118.JPG?alt=media&token=d039db34-f414-489d-bb9b-dfd3bf811354"
+  },
+  {
+    "caption" : "松雀鷹與鳳頭蒼鷹是大家常搞混的兩種猛禽，相較於鳳頭蒼鷹，松雀鷹的行蹤更為隱密些，且較少出現在都會區。體型上松雀鷹比鳳頭蒼鷹來的瘦小許多，但其性格卻相當兇悍，常主動攻擊其他猛禽，又有\"鷹仔虎\"之稱。松雀鷹與鳳頭蒼鷹的差異還包括鳴叫聲、嘴喙比例較小、跗蹠較細、腳趾較長、初級飛羽突出數目以及飛行行為等不同之處，此外其外側尾羽與其他尾羽異紋的特徵，也是跟鳳頭蒼鷹辨識的要點",
+    "name" : "松雀鷹",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FIMG_0152.JPG?alt=media&token=306cae46-7feb-40ab-a311-32773a927d18"
+  },
+  {
+    "caption" : "鉛色水鶇全身以灰藍色為主，牠們的性別可以由尾巴的顏色來分辨：雄鳥自腰部到尾部為棗紅色；而雌鳥除了尾羽基部為白色之外，其餘皆為黑褐色。鉛色水鶇的幼鳥體色大致上和雌鳥相仿，唯身上密佈著部多白色的小斑點，可以和雌性成鳥區別。鉛色水鶇的主要食物是昆蟲，牠們每每站立在展望良好的石頭上，當看到有飛行中的昆蟲，如蜉蝣、石蛉、石蠶蛾經過，或小蟲漂在水面上時，牠們就會迅速的起飛捕食，再返回原地，動作可說相當敏捷。",
+    "name" : "鉛色水鶇-母",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FIMG_0397.JPG?alt=media&token=e1e43539-155c-4cee-beaa-525fe740f021"
+  },
+   {
+    "caption" : "夏季在西伯利亞的貝加爾湖及黑龍江流域一帶繁殖，冬天移棲江南、台灣、日本及青康藏高原。每年十月以後會出現在台灣，在台灣為不普遍的冬候鳥食物以昆蟲等無脊椎動物為主，兼食少數漿果及種子。出現於出現於平地至中海拔1500公尺左右稀疏林間或空曠地帶之灌叢或獨立物上，通常單獨活動，不時抖動尾羽，飛下捕食地面的獵物。",
+    "name" : "黃尾鴝-母",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FIMG_0179.JPG?alt=media&token=aff06b0d-8196-4481-bcf4-824e45b5436f"
+  },
+ {
+    "caption" : "分布於低至中海拔山澗、溪流附近或森林邊緣陰濕環境。通常單獨出現在溪澗、水邊或林緣地帶，地域性相當明顯，生性機警且容易受到驚擾。主要以蚯蚓、魚蝦、兩棲爬蟲或小型昆蟲為食。",
+    "name" : "紫嘯鶇",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FIMG_8312.JPG?alt=media&token=95623676-b74e-4da1-abbe-5b2a215ba3b1"
+  },
+   {
+    "caption" : "1.體長22-26公分。2.虹膜暗紅色，嘴喙呈鉛灰色。3.角羽明顯，身體大致呈灰黑色，有黑褐色斑紋。夜行性，黃昏後活動，主食為昆蟲、小型鳥類、哺乳類及兩爬類。春夏夜晚常鳴叫。近年來校園、都會公園常有紀錄",
+    "name" : "領角鴞",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FIMG_8162.JPG?alt=media&token=b928fc07-c6b8-4653-ac1b-c2b4b16ae0f7"
+  },
+  {
+    "caption" : "「鵂鶹」，音同「休留」，體型最嬌小，台灣鴟鴞科中無「鳥」能出其右，為特有亞種，保育等級Ⅱ，屬珍稀野生動物。牠的嬌俏身影不但是長鏡頭注目的焦點，也讓牠成為出色的模仿高手。不過，被人類視為珍寶的鵂鶹，對其他鳥群來說，竟是避之唯恐不及的惡夢，原因何在？大約只有一個拳頭大小的鵂鶹，是台灣體型最小的貓頭鷹。千萬可別被牠可愛討喜的模樣給欺騙了，鵂鶹不折不扣是個兇悍無比的狠角色。每次出現，都會立刻引起林間活動的鳥群們一陣騷動，陷入混亂不安的場面。個頭迷你的鵂鶹，即使是碰到比自身體型大上許多的藪鳥，一樣可以輕易制伏。主要獵物除了山雀科的小型鳥類之外，也會捕捉昆蟲、蛙類和蜥蜴等其它小動物。",
+    "name" : "鵂鶹",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2Fi11.jpg?alt=media&token=f2cbdf06-ec3c-46d2-a1d0-016c9977bf65"
+  },
+   {
+    "caption" : "黃山雀棲息在二千公尺以下的闊葉林，是台灣特有種鳥類中唯一的山雀科鳥類，常單獨或成對的在高大的樹頂鳴唱，聲音極為優美響亮，常可由聲音找到牠們的蹤影。牠有青背山雀般黃綠的羽色，但卻多了一頂黑色羽冠。在台灣的四種山雀中，其體型較壯碩，當與其他種混群出現時，極容易找出牠來，然族群數量不多，是較不普遍的留鳥。繁殖期時雄鳥的聲音非常婉轉悅耳，有時甚至可持續達一分鐘，非繁殖期時則不常鳴叫，也較不易發現。",
+    "name" : "黃山雀",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FIMG_2568.jpg?alt=media&token=0575a1a2-61ff-42e1-b0c2-b0707798c8d9"
+  },
+   {
+    "caption" : "對於喜愛賞鳥的人們來說，在台灣的五百多種鳥類中，其中有三種是令人神往而不容易遇見的，被稱為「白藍帝」──這並不是一種酒，而是「白頭鶇(或白喉笑鶇)」、「藍腹鷴」與「帝雉」。其中屬於特有亞種的白頭鶇是一種留鳥──「特有亞種」的意思是這種鳥和地球上其他地方如南洋群島、新幾內亞及南太平洋諸島，有50多個亞種的同種鳥型態和行為差異頗大，而其經過長時間的演化後，有機會變成獨特的新種。另外，所謂「留鳥」，就是一直住在台灣。",
+    "name" : "白頭鶇",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FIMG_2718.jpg?alt=media&token=04872bb5-4d2f-464c-9c88-dd2a0daa9816"
+  },
+  {
+    "caption" : "頭胸、背尾為藍黑色，腹白色，翼藍黑色，有白色翼帶。食性以昆蟲、植物種子及果實為主食。多單獨活動，停棲時大多身體挺直，常棲息於樹林冠木叢、草叢地帶或其附近之林緣地帶，築巢建築物岩石縫或樹上灌木叢中，雛鳥為晚熟性。出現於公園、樹林。台灣多數族群為籠逸鳥於野外所繁殖。",
+    "name" : "鵲鴝",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FIMG_1569.jpg?alt=media&token=5570a280-9832-4616-a67d-334f43178534"
+  },
+  {
+    "caption" : "台灣藍鵲的活動範圍侷限在低海拔山區，因其具有不怕人的特性，常可見牠們在山區的人家或小型村落，以及經人工開墾的竹林、果園、農地等地區活動。常在闊葉林區或次生林地帶棲息，由於體型大，在飛行時僅作短距離移動，常見一隻接一隻的滑行過山谷，並發出金屬般「ㄎ一ㄤ、ㄎ一ㄤ」的鳴叫聲。其社會行為亦相當複雜，同群藍鵲間有相互乞食和理毛的行為。牠們也會共同合作抵禦掠食者的威脅，對於生殖期間闖入築巢區的人類，也會肆無忌憚的發動攻擊，直到威脅消失為止。孵卵、育雛時可能有幫手制度。",
+    "name" : "台灣藍鵲",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FIMG_8314.JPG?alt=media&token=02e8e3f2-2be6-4d30-ac44-741ae6a24227"
+  },
+   {
+    "caption" : "藪鳥全身大致為橄黃色，頭頂灰黑色，額黃色，腳和嘴為鐵灰色，眼前下方有一橙黃色斑，猶如一顆黃色的美人痣斑，新出版故名「黃痣藪眉」。藪鳥是台灣中低海拔常見的鳥， 雌雄同色，愛鳴唱是畫眉科鳥類的傳統，當走在山區的產業道路旁或小徑時，耳邊不時傳來藪鳥的叫聲，聽起來像「急－救兒」溪頭的藪鳥多的像老鼠般隨處亂竄，不但十分易見，而且不怕人，其身材圓胖、顏色像地瓜，所以鳥友戲謔牠為「蕃薯鳥」。",
+    "name" : "藪鳥",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2Fi14.jpg?alt=media&token=a8501dad-476f-40dc-ace9-0c571e26d166"
+  },
+  {
+    "caption" : "長14～15公分，雄鳥頭部至上胸橘紅色，背部及尾羽紅褐色，翼褐色，下胸及脅銀灰色，腹部污白色，腳褐色。雌鳥大致似雄鳥，但羽毛較暗淡、下胸及脅偏褐色。常單獨活動，遷移期偶成小群，非繁殖期通常安靜無聲，偶有似敲石頭的低沉喉音。",
+    "name" : "日本歌鴝",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FIMG_0295.jpg?alt=media&token=bb10daed-0d38-4508-acae-2c86514132ed"
+  },
+   {
+    "caption" : "出現於中低海拔到海岸地 帶的高草叢、蘆葦叢和灌叢及稻田， 與灰頭鷦鶯的棲地有重疊，但分佈 更為廣泛。台江地區於各種適合 棲地經常可見，雄鳥會站立於高 處高舉尾羽鳴唱，叫聲為單調平 緩之『滴、滴、滴』，以小型無 脊椎動物為食。",
+    "name" : "褐頭鷦鶯",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN5063.jpg?alt=media&token=d178cb3f-a35d-4dba-8c5b-579607565a58"
+  },
+  {
+    "caption" : "出現於河口、海岸、沼澤、溪澗及湖邊的普遍冬候鳥，停留時間長，幾乎終年可見，但6月至7月紀錄最少。很少高飛，常輕鬆拍動翅膀在水面上空滑過。單獨棲息於河床石塊、船舷或堤防上，除遷移外很少成群活動。單獨活動，沿水邊快速行走覓食，食物以昆蟲為主。頭部不時上下點動，行進及停止時尾部會上下擺動。如果有同類接近活動領域，會將其驅離。繁殖於西伯利亞、滿洲及日本。雌雄鳥同色。",
+    "name" : "磯鷸",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN5055.jpg?alt=media&token=7aa4ec48-24b1-49f6-9251-7c90d24674af"
+  },
+ {
+    "caption" : "上傳完畢請伯勞鳥算是台灣侯鳥中最出名的一種了，因為往年在恆春地區烤小鳥，出售的就是紅尾伯勞，在分類學上牠屬於伯勞科，身長約18公分，最明顯的特徵就是在眼睛周圍有明顯的黑色，有如戴上黑眼罩一般，另外該注意的是，雖然叫牠紅尾伯勞，但牠的尾巴僅有些許的紅褐色，並不是整個都是紅色的。\n\n紅尾伯勞繁殖於日本、韓國、中國東北等地區，每年八、九月間有大族群過境恒春半島，牠的叫聲沙啞難聽，也因此有人就拿伯勞的叫聲來比喻人說話難聽，孟子梁惠王篇中「南蠻鴃舌之人」就是這個意思，而其中的「鴃」也就是伯勞的古名。進入觀賞",
+    "name" : "紅尾伯勞",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN4883.jpg?alt=media&token=57dd18b4-8519-4cf7-8372-094235028c32"
+  },
+   {
+    "caption" : "白環鸚嘴鵯，臺灣話稱石鸚哥(tsio̍h-ing-ko)、林投雞或林投公，為小型鵯科鳥類，體長大概十五公分，因頸部有一明顯白環而得名，喙部米黃色，頭部黑色，身體呈黃綠色。生活在中低海拔的山區，但是從平原丘陵地到山上都可發現牠們的蹤跡。叫聲為：「追丟-對 追丟-對」。平常僅停棲在樹枝、樹幹。個性非常隱密。",
+    "name" : "白環鸚嘴鵯",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FIMG_0745.JPG?alt=media&token=9035ebee-33c1-4243-84a0-200ec7011d35"
+  },
+   {
+    "caption" : "短翅樹鶯(遠東樹鶯)：Manchurian Bush-Warbler.\n短翅樹鶯，全長約16公分，是普遍冬候鳥，繁殖於西伯利亞東南部、東三省、朝鮮等地方，每年九月來臨，次年四月北歸。鳴聲清脆婉轉，以昆蟲為食。\n短翅樹鶯分為兩個獨立種：遠東樹鶯Manchurian Bush-Warbler和日本樹鶯Japanese Bush Warbler",
+    "name" : "短翅樹鶯",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FIMG_0743.JPG?alt=media&token=215c0f38-4713-4188-a094-21c696599b63"
+  },
+  {
+    "caption" : "田鷸是台灣地區的普遍冬候鳥，通常單獨或成小群活動於水田、沼澤地帶。在鷸科鳥類中，田鷸屬水鳥擁有絕佳的保護色：牠們的身上大致以黑色、褐色為主，再間雜了許多黃色、白色的條紋及斑點，當這樣外型的鳥類一動不動地蟄伏在枯黃的水邊草叢或收割過的稻田中時，真是讓人幾乎無法感覺到牠們的存在。",
+    "name" : "田鷸",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FIMG_0690.JPG?alt=media&token=658b6f49-e625-4cc9-840f-6403be35eb59"
+  },
+  {
+    "caption" : "白領翡翠活動於沿海或近水開闊區域及淡水渠道、蘆葦叢、森林、河流、湖泊沿岸、瀉湖、河口周圍的灌木叢、包括花園、人工林及城鎮。棲於岩石或樹上。主要食物是羅非魚、鯉、蟹，蛙和水生昆蟲。",
+    "name" : "白領翡翠",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2F3FB1F96B-1C8A-48FE-ABFD-5AD7009F7723.jpeg?alt=media&token=90cbc5d7-7f1f-4702-a393-c5e193d5cd1c"
+  },
+  {
+    "caption" : "白腹秧雞以嘴喙在泥巴、淺水灘中找尋食物或直接目視啄食，也在地上、低灌木和小樹叢裡找尋食物，主要吃昆蟲、小魚和種子. 。\n許多秧雞科鳥類習性是相當隱密的，但白腹秧雞則常大方出現，在清晨和黃昏時會發生\"苦啊苦啊\"大叫聲，變得非常吵。",
+    "name" : "白腹秧雞",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN6786.jpg?alt=media&token=2712e22a-a1a5-43d0-8373-649e494281da"
+  },
+   {
+    "caption" : "水雉比較喜愛亞熱帶濕地環境，所以在南部的荷田或菱角池塘裡，較容易看到牠們的蹤影，牠們喜歡在長滿布袋蓮、浮水植物或菱田的池塘裡活動。水雉的進化，使得水雉擁有一雙像雪鞋一樣可以行走在浮葉的腳 (因為特別長的腳趾使牠能將重量分散到大於本身，輕薄的浮水葉片上，因此水雉在浮葉上如履平地)。就算是已會飛的亞成鳥、成鳥，在池裡也以行走為主，只有在遠距離覓食或受干擾才會飛行前往，多數時間步行。)，牠們修長的腿趾漫步在沼地裡顯得優美多姿，所以此地的民眾另稱牠們「凌波仙子」。在日據時代，牠是被列為天然野生紀念物之一而受到嚴格的保護呢！",
+    "name" : "水雉",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN7376.JPG?alt=media&token=8698b845-9738-4ee4-841a-5d8c1bcc2533"
+  },
+   {
+    "caption" : "這是美麗的翠翼鳩公鳥。分辨公母的方式最主要的就是他們眼睛旁邊那道白眉。公鳥的白眉較粗且明顯，母鳥的則較細。他們屬於珍貴稀有的留鳥，食性為種子跟果實，主要棲息在林地，也就是森林的邊緣。",
+    "name" : "翠翼鳩",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN7484.JPG?alt=media&token=c90f0d9e-5f45-4551-89be-2ed2cfffb258"
+  },
+   {
+    "caption" : "灰鷽又名灰頭灰雀，為雀科灰雀屬的鳥類。分布於錫金、不丹、印度、台灣以及中國的西北、華北、華中、西南山地、西藏等地，常見於海拔1500-4000m 的高山帶、棲息於亞熱帶常綠闊葉林、針闊混交林、高山草甸及裸岩等生境以及常見于山頂、高山樹冠上。該物種的模式產地在錫金。",
+    "name" : "灰鷽",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN7810.JPG?alt=media&token=a29f1574-016a-454f-9695-98f1143ec61f"
+  },
+   {
+    "caption" : "酒紅朱雀是較容易仔細觀察的中高海拔鳥種。 雌雄的顏色都較濃重，雄鳥除了翅膀、尾巴有黑褐色的羽毛，眼上的白色眉線外，一身都是艷麗如紅酒般的顏色，雌鳥整體大約是暗橄欖褐色。 主要在地表如草叢、道路、森林邊緣活動，鳴聲「吱、吱、吱」，簡單而短促。傳完畢請進入觀賞",
+    "name" : "酒紅朱雀",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN7846.jpg?alt=media&token=bf9b3174-4202-4433-b3e6-97b675b57d51"
+  },
+   {
+    "caption" : "栗背林鴝為鶲科林鴝屬的鳥類，別名阿里山鴝，是台灣的特有物種。分布於台灣本島，一般生活于山區海拔2200－3500米間的林下底叢中，常出現於林邊小徑的路上或灌木的頂枝上，罕見於開曠的地區。該物種的模式產地在台灣玉山。傳完畢請進入觀賞",
+    "name" : "栗背林鴝",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN7861.JPG?alt=media&token=96bda0ab-5f3a-4611-958a-c4d67268abf8"
+  },
+   {
+    "caption" : "金翼白眉，又名玉山噪鶥或台灣噪眉，臺灣話稱四眉，是噪鶥屬的一種，分布於台灣。該物種的保護狀況被評為無危。 金翼白眉特徵在於暗棕色的臉頰有顯眼的白色眉線及顎線，翼和尾羽為藍灰與金黃色。身體大致呈棕褐色，布滿模糊鱗狀斑，腹灰褐色，無斑。體型稍大，只適合短距離滑翔，動作靈敏，常被賞鳥人暱稱「高山老鼠」。",
+    "name" : "金翼白眉",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN7866.JPG?alt=media&token=2285df20-903d-4b00-bf37-432292eccb68"
+  },
+   {
+    "caption" : "岩鷚科分布於歐亞大陸，大部\n份生活於山區森林或灌木叢。外觀多灰褐色佈有條紋，於頭上有\n濃的花紋，腹面灰白，色調與環境融合。嘴基寬闊，往先端急劇\n尖銳，於側面膨大，橫切面圓形。這科主要特徵是具獨一無二的\n嘴喙形狀，在覓食時會看見牠們用嘴喙快速翻開樹葉，攝食小蟲\n或種子。",
+    "name" : "岩鷚",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN7886.JPG?alt=media&token=9ff890aa-4581-41d6-a08f-4817a3a31a7d"
+  },
+   {
+    "caption" : "黑背燕尾（学名：Enicurus immaculatus，英文名：Black-backed Forktail）是鹟科燕尾属的鸟类，俗名中国黑背燕尾、中国燕尾、山燕子。分布于中国南疆诸邻国、马来西亚、印度尼西亚以及中国的甘肃、陕西、河南、广东、海南、四川、云南等地，一般栖息于溪流水边的岩石上、或在山间急流附近以及也见停息在沿溪的树丛间或村寨中的水沟边。该物种的模式产地在印度尼西亚的爪哇。",
+    "name" : "黑背燕尾",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN8672%20(2).JPG?alt=media&token=714dc17a-f006-45c9-bfc3-b8eca6daee2b"
+  },
+   {
+    "caption" : "星鴉身長34公分,嘴腳均為黑色,體色大致為黑褐色,背部及喉、胸、頸部上有明顯白\n色狀班點,是臺灣特有亞種,喜歡在中、高海拔的針樹林中活動,食物主食為松果子,在\n多餘食物未能食用完時,會將松果子儲藏於樹縫中,是相當聰明的一種鳥類...觀察時,\n除了吃松果子外,牠總是喜歡站立在松樹頂上,幾乎每次遇到時都是高高在上",
+    "name" : "星鴉",
+    "url" : "https://firebasestorage.googleapis.com/v0/b/firstapp-6e2b3.appspot.com/o/birdimage%2FDSCN8929%20(2).JPG?alt=media&token=3d7fd052-85fe-4a81-b817-f28f3dc33789"
+  }
+]
